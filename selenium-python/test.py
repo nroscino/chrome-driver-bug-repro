d026dfd179f9c16beec32ec90849d3a350179ee0
Nicholas Roscino <nroscino@google.com>
2026-01-08 15:06:55 +0000

    Attempt of bug reproduction

--- selenium-python/test.py
+++ selenium-python/test.py
@@ -28,10 +28,10 @@ def driver():
     # By default, the test uses the latest stable Chrome version.
     # Replace the "stable" with the specific browser version if needed,
     # e.g. 'canary', '115' or '144.0.7534.0' for example.
-    browser_version = "stable"
+    browser_version = "113"
 
     options = Options()
-    options.add_argument("--headless")
+    options.add_argument("--headless=new")
     options.add_argument("--no-sandbox")
     options.browser_version = browser_version
 
@@ -54,5 +54,15 @@ def test_should_be_able_to_navigate_to_google_com(driver):
 
 @pytest.mark.timeout(TIMEOUT)
 def test_issue_reproduction(driver):
-    """Add test reproducing the issue here."""
-    pass
+    """Reproducing the local storage timeout issue.
+    The bug report suggests that calling execute_script to set an item in localStorage
+    causes a timeout in Chrome 107+.
+    """
+
+    driver.get('https://www.google.com')
+    driver.execute_script(f"window.localStorage.setItem('fp',{'123'});")
+    
+    # Verify the value is set correctly.
+    val = driver.execute_script("return window.localStorage.getItem('fp');")
+
+    assert val == "123"
