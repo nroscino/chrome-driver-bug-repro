bc4a772ad54a3fe0109d77bf814838cec8d27441
Nicholas Roscino <nroscino@google.com>
2026-01-08 17:34:56 +0000

    Reproduced bug 425906330

--- selenium-python/test.py
+++ selenium-python/test.py
@@ -14,6 +14,7 @@
 
 import logging
 import pytest
+import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
@@ -22,6 +23,10 @@ from selenium.webdriver.chrome.service import Service
 # Give 5 minutes to install everything.
 TIMEOUT = 5 * 60 * 1000
 
+def log_request(request):
+    print(f"Request to: {request.url}")
+    print("Headers:", request.headers)
+    request.continue_request()
 
 @pytest.fixture(scope="module")
 def driver():
@@ -30,19 +35,21 @@ def driver():
     # e.g. 'canary', '115' or '144.0.7534.0' for example.
     browser_version = "stable"
 
-    options = Options()
-    options.add_argument("--headless")
-    options.add_argument("--no-sandbox")
-    options.browser_version = browser_version
+    stableOption = Options()
+    stableOption.add_argument("--headless")
+    stableOption.add_argument("--no-sandbox")
+    stableOption.enable_bidi = True
+    stableOption.browser_version = browser_version
+    # Enable WebDriver BiDi
+    stableOption.set_capability("webSocketUrl", True)
 
     service = Service(service_args=["--log-path=chromedriver.log", "--verbose"])
 
-    driver = webdriver.Chrome(options=options, service=service)
+    driver = webdriver.Chrome(options=stableOption, service=service)
 
     yield driver
 
     driver.quit()
-
 
 @pytest.mark.timeout(TIMEOUT)
 def test_should_be_able_to_navigate_to_google_com(driver):
@@ -51,8 +58,21 @@ def test_should_be_able_to_navigate_to_google_com(driver):
     logging.info(driver.title)
     assert driver.title == "Google"
 
-
 @pytest.mark.timeout(TIMEOUT)
-def test_issue_reproduction(driver):
-    """Add test reproducing the issue here."""
-    pass
+def test_from_issue(driver):
+    options = webdriver.ChromeOptions()
+    options.set_capability("webSocketUrl", True)
+    options.enable_bidi = True
+    options.browser_version = "121"
+    customDriver = webdriver.Chrome(options=options)
+
+    customDriver.network.add_request_handler(
+        event="before_request",
+        callback=log_request,
+    )
+
+    customDriver.get("https://www.google.com")
+
+    time.sleep(5)
+    assert customDriver.title == "Google"
+    customDriver.quit()
